3202154520dc09ed3d23f8a7c3a969f872b6f092
ABIOLA ELIJAH ALALADE <72648063+abhihola@users.noreply.github.com>
2025-03-19 16:52:35 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require('express');
-require('dotenv').config();
-require('./bot'); // Runs the Telegram bot
+const bot = require('./bot'); // Import bot.js
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send("Telegram bot is running...");
+    res.send('Telegram bot is running...');
 });
 
 app.listen(PORT, () => {
