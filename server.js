cfe745d75b3da6976f2402ea5145b437595cf354
ABIOLA ELIJAH ALALADE <72648063+abhihola@users.noreply.github.com>
2025-04-08 19:19:47 +0100

    Update server.js

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ botTokens.forEach(async (token, index) => {
 });
 
 // Render Deploy Hook URL (Auto-refresh every 5 minutes)
-const DEPLOY_HOOK_URL = "https://api.render.com/deploy/srv-cvo0gafgi27c73bq34hg?key=b8UGNurn1to";
+const DEPLOY_HOOK_URL = "https://api.render.com/deploy/srv-cvjedep5pdvs73f09ff0?key=9eJT7BFAnQI";
 
 async function refreshBots() {
     try {
